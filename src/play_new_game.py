5de0cf6e3d9dcc479cf3ad0f1ac8cfa3f4e65b98
Josh Campbell <jcampbell84@gmail.com>
2017-07-01 11:56:49 -0700

    change shouldEndSession to string

--- src/play_new_game.py
+++ src/play_new_game.py
@@ -17,7 +17,7 @@ def play_new_game():
     "as fast as you can.  Starting in...  3... 2... 1..."
     questions = pick_random_questions(2, 2)
     speech_output = new_game_message + questions[0]['question']
-    should_end_session = False
+    should_end_session = "false"
     attributes = {
         "questions": questions,
         "score": 0,
