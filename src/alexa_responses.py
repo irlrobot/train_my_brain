9d0b13760092a9a0e7de5a81914300b2d13bce36
Josh Campbell <jcampbell84@gmail.com>
2017-07-01 17:16:31 -0700

    use bucket url instead

--- src/alexa_responses.py
+++ src/alexa_responses.py
@@ -40,8 +40,10 @@ def speech_with_card(tts, attributes, should_end_session, card_title, card_text)
                 "title": card_title,
                 "text": card_text,
                 "image": {
-                    "smallImageUrl": "https://aws.userdel.com/train_that_brain_small_card.jpg",
-                    "largeImageUrl": "https://aws.userdel.com/train_that_brain_large_card.jpg"
+                    "smallImageUrl":
+                        "https://s3.amazonaws.com/aws.userdel.com/train_that_brain_small_card.jpg",
+                    "largeImageUrl":
+                        "https://s3.amazonaws.com/aws.userdel.com/train_that_brain_large_card.jpg"
                 }
             }
         }
