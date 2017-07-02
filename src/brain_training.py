27f5ae46af29e5e46cd9be11f6d5350242cd69d7
Josh Campbell <jcampbell84@gmail.com>
2017-07-01 17:16:17 -0700

    add more questions and categories

--- src/brain_training.py
+++ src/brain_training.py
@@ -6,21 +6,21 @@ github.com/irlrobot/train_that_brain
 """
 QUESTIONS = [
     {
-        "alphabet_position": [
+        "word_jumble": [
             {
                 "id": 1,
-                "question": "Where in the alphabet is letter A?",
-                "answer": "1"
+                "question": "What word can you spell with these letters:  S... N... W... I...",
+                "answer": "Wins"
             },
             {
                 "id": 2,
-                "question": "Where in the alphabet is letter B?",
-                "answer": "2"
+                "question": "What word can you spell with these letters:  O... B... I... L...",
+                "answer": "Boil"
             },
             {
                 "id": 3,
-                "question": "Where in the alphabet is letter C?",
-                "answer": "3"
+                "question": "What word can you spell with these letters:  E... B... U... C...",
+                "answer": "Cube"
             }
         ]
     },
@@ -59,6 +59,57 @@ QUESTIONS = [
                 "id": 3,
                 "question": "What does 10... times... 2... plus... 5... minus... 1... equal?",
                 "answer": "24"
+            }
+        ]
+    },
+    {
+        "memory_game": [
+            {
+                "id": 1,
+                "question": "Sara went to the movies on Thursday with her friend Brandon...  " + \
+                    "The movie they saw was called Thermal Unrest... They both wore blue " + \
+                    "shirts...  What day did they go to the movie?",
+                "answer": "Thursday"
+            },
+            {
+                "id": 2,
+                "question": "Josh bought two dozen roses for his wife Bridget on Sunday... " + \
+                    "Monday is their third anniversary and they're going to an italian" + \
+                    "retaurant called Gino's in Chicago...  How many dozen roses did Josh buy?",
+                "answer": "2"
+            },
+            {
+                "id": 3,
+                "question": "Justin and Tara just got back from a 9 day trip to " + \
+                    "Green Bay.  The trip was for a family reunion where " + \
+                    "they saw 31 relatives, including Tara's cousin Joey from Detroit" + \
+                    "who Tara hadn't seen in 11 years.  What city did Justin and Tara " + \
+                    "just return from?",
+                "answer": "Green Bay"
+            }
+        ]
+    },
+    {
+        "simple_trivia": [
+            {
+                "id": 1,
+                "question": "What is the capital of Alaska?",
+                "answer": "Juneau"
+            },
+            {
+                "id": 2,
+                "question": "How many states border Florida?",
+                "answer": "2"
+            },
+            {
+                "id": 3,
+                "question": "What is a Geiger Counter used to detect?",
+                "answer": "Radiation"
+            },
+            {
+                "id": 4,
+                "question": "Babe Ruth is associated with which sport?",
+                "answer": "Baseball"
             }
         ]
     }
