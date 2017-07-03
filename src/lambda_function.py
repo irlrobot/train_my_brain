ddf714a6f08d29bb13de5a04b96bd5422929b796
Josh Campbell <jcampbell84@gmail.com>
2017-07-02 18:53:25 -0700

    move ened message out to alexa_response and add debugging for intents

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -6,10 +6,9 @@ github.com/irlrobot/train_that_brain
 """
 from __future__ import print_function
 import json
-from random import randint
 from play_new_game import play_new_game
 from handle_answer_request import handle_answer_request
-from alexa_responses import speech
+from alexa_responses import play_end_message
 
 def lambda_handler(event, _context):
     """main function for AWS Lambda"""
@@ -48,26 +47,20 @@ def on_intent(event_request, session):
     intent_name = event_request['intent']['name']
 
     if intent_name == "AnswerIntent":
+        print("=====AnswerIntent fired...")
         return handle_answer_request(intent, session)
     if intent_name == "GameIntent":
+        print("=====GameIntent fired...")
         return play_new_game()
-    if intent_name == "AMAZON.StopIntent" or "AMAZON.CancelIntent":
-        tts = "Thanks for playing.  Please leave a review and let us know what you thought."
-        return speech(tts, {}, True)
+    if intent_name == "AMAZON.StartOverIntent" or "AMAZON.YesIntent":
+        print("=====StartOverIntent or YesIntent fired...")
+        return play_new_game()
+    if intent_name == "AMAZON.StopIntent" or "AMAZON.CancelIntent" or "AMAZON.NoIntent":
+        print("=====StopIntent, CancelIntent, or NoIntent fired...")
+        return play_end_message()
 
 def on_session_ended(event_request, session):
     """when the user ends the session intentionally or timeout"""
     print("=====on_session_ended requestId=" + event_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-
-    standard_message = "Thanks for playing Train That Brain."
-    review_message = "Please leave a review and let us know what you thought "\
-        "of Train That Brain."
-
-    # don't always ask for a review
-    if randint(1, 2) == 1:
-        tts = standard_message + review_message
-    else:
-        tts = standard_message
-
-    return speech(tts, {}, True)
+    return play_end_message()
