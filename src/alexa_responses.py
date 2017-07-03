894be72ea7f6a051ec851ccb49f805a0e91ad7cc
Josh Campbell <jcampbell84@gmail.com>
2017-07-02 18:53:51 -0700

    adds play_end_message

--- src/alexa_responses.py
+++ src/alexa_responses.py
@@ -5,6 +5,7 @@ v1.0.0
 github.com/irlrobot/train_that_brain
 """
 from __future__ import print_function
+from random import randint
 
 def speech(tts, attributes, should_end_session):
     '''build speech output'''
@@ -63,3 +64,18 @@ def speech_with_card(tts, attributes, should_end_session, card_title, card_text)
 
     print("=====response back to alexa service:  \n" + str(response))
     return response
+
+def play_end_message():
+    """play a standard message when exiting the skill"""
+    standard_message = "Thanks for playing Train That Brain.  Play daily to keep "\
+        "your mind muscles strong."
+    review_message = "Please leave a review and let us know what you thought "\
+        "of Train That Brain."
+
+    # don't always ask for a review
+    if randint(1, 3) == 1:
+        tts = standard_message + review_message
+    else:
+        tts = standard_message
+
+    return speech(tts, {}, True)
