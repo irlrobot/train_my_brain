bf03b466e20615610b276939659fe0ca135df278
Josh Campbell <jcampbell84@gmail.com>
2017-07-14 11:57:45 -0700

    change question 67 and pull repeat category for now

--- src/brain_training.py
+++ src/brain_training.py
@@ -478,11 +478,11 @@ QUESTIONS = [
     },
     {
         "id": 67,
-        "question": "Zach's 25th birthday is on July 13th... "\
-            "Melissa's 24th birthday is on July 15th..." \
+        "question": "Zach's 27th birthday is on July 13th... "\
+            "Melissa's 26th birthday is on July 15th..." \
             "They'll celebrate both birthday's on the 17th..." \
             "How old will Melissa be?",
-        "answer": "24",
+        "answer": "26",
         "category": "memory_game"
     },
     {
@@ -496,71 +496,5 @@ QUESTIONS = [
         "question": "What word can you spell with these letters:  U... M... P... L... P...",
         "answer": "Plump",
         "category": "word_jumble"
-    },
-    {
-        "id": 70,
-        "question": "Repeat these words back to me... Epic... Bird... Taco...",
-        "answer": "Epic Bird Taco",
-        "category": "repeat"
-    },
-    {
-        "id": 71,
-        "question": "Repeat these words back to me... Map... Computer... Fish...",
-        "answer": "Map Computer Fish",
-        "category": "repeat"
-    },
-    {
-        "id": 72,
-        "question": "Repeat these words back to me... French... Boat... Yesterday...",
-        "answer": "French Boat Yesterday",
-        "category": "repeat"
-    },
-    {
-        "id": 73,
-        "question": "Repeat these words back to me... November... Star... Dream...",
-        "answer": "November Star Dream",
-        "category": "repeat"
-    },
-    {
-        "id": 74,
-        "question": "Repeat these words back to me... Drum... Animal... Mighty...",
-        "answer": "Drum Animal Mighty",
-        "category": "repeat"
-    },
-    {
-        "id": 75,
-        "question": "Repeat these words back to me... Saxophone... Analysis... War...",
-        "answer": "Saxophone Analysis War",
-        "category": "repeat"
-    },
-    {
-        "id": 76,
-        "question": "Repeat these words back to me... Party... Fire... Drink... Water...",
-        "answer": "Party Fire Drink Water",
-        "category": "repeat"
-    },
-    {
-        "id": 77,
-        "question": "Repeat these words back to me... Answer... Phone... Cancel... Shower...",
-        "answer": "Answer Phone Cancel Shower",
-        "category": "repeat"
-    },
-    {
-        "id": 78,
-        "question": "Repeat these words back to me... Think... Observe... Sleep... Pizza...",
-        "answer": "Think Observe Sleep Pizza",
-        "category": "repeat"
-    },
-    {
-        "id": 79,
-        "question": "Repeat these words back to me... Pressure... Pants... Advance... Game...",
-        "answer": "Pressure Pants Advance Game",
-        "category": "repeat"
-    },
-    {
-        "id": 80,
-        "question": "Repeat these words back to me... Moment... Win... Heat... Growth...",
-        "answer": "Moment Win Heat Growth",
-        "category": "repeat"
     }
 ]
