08c3bb76e9d4a6de1317ba83cf553440a6fd06c8
Josh Campbell <jcampbell@zillowgroup.com>
2020-07-15 21:56:05 -0700

    massive refactor part uno :fire:

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,97 +1,83 @@
 #!/usr/bin/env python
 """
 Train My Brain
-github.com/irlrobot/train_my_brain
+An Alexa Skill by Josh Campbell
 """
-from __future__ import print_function
 import json
+import logging
 from play_new_game import play_new_game
 from handle_answer_request import handle_answer_request
 from alexa_responses import play_end_message, speech
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 def lambda_handler(event, _context):
-    """main function for AWS Lambda"""
-    print('=====lambda handler started...')
-    print(json.dumps(event))
+    """AWS Lambda Entry Point"""
+    logger.debug('=====lambda handler started...')
+    logger.debug(json.dumps(event))
+    logger.info("Request ID: {}\nSession: {}".format(
+            event['request']['requestId'], event['session']))
 
-    if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
-        # this will trigger if a one shot is used
-        if event['request']['type'] == "IntentRequest":
-            return on_launch(event['request'], event['session'])
-
+    # If a one-shot was used to start a new game
+    if event['session']['new'] and event['request']['type'] == "IntentRequest":
+        return play_new_game(replay=False)
     if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
+        return play_new_game(replay=False)
     if event['request']['type'] == "IntentRequest":
-        return on_intent(event['request'], event['session'])
+        return on_intent(event['request']['intent'], event['session'])
     if event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
+        return play_end_message()
 
-def on_session_started(session_started_request, session):
-    """when starting a new session"""
-    print("=====on_session_started requestId:  " +
-          session_started_request['requestId'] + ", sessionId=" +
-          session['sessionId'])
-
-def on_launch(event_request, session):
-    """when customer launches the skill via modal"""
-    print("=====on_launch requestId:  " + event_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-    return play_new_game(False)
-
-def on_intent(event_request, session):
-    """when customer launches the skill via modal"""
-    print("=====on_intent requestId:  " + event_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-
-    intent = event_request['intent']
-    intent_name = event_request['intent']['name']
-    print("=====intent is: " + intent_name)
+    # todo politely bug out and log a crash dump if we hit none of the above
+    
+def on_intent(intent, session):
+    """Router for all IntentRequest's"""
+    intent_name = intent['name']
+    logger.debug("=====IntentRequest: " + intent_name)
 
     if intent_name == "AnswerIntent":
-        print("=====AnswerIntent fired...")
-        if 'attributes' in session:
-            if 'questions' in session['attributes']:
-                return handle_answer_request(intent, session)
+        logger.debug("=====AnswerIntent fired...")
+        if 'attributes' in session and 'questions' in session['attributes']:
+            return handle_answer_request(intent, session)
 
-        # we probably got here because user said something other than
-        # yes or no after asking if they wanted to play the game again
-        print("=====no attributes ending game")
+        # We probably got here because the user said something other than
+        # yes or no after asking if they wanted to play the game again.
+        logger.info("=====No attributes in session, ending game!")
         return play_end_message()
+
     if intent_name == "GameIntent":
-        print("=====GameIntent fired...")
-        # if there's a session and we're in a game treat this as an answer
-        # unfortunately it will be wrong but it's better than starting over
-        if 'attributes' in session:
-            if session['attributes']['game_status'] == "in_progress":
+        logger.debug("=====GameIntent fired...")
+        # If there's a session and we're in a game, treat this as an answer.
+        # Unfortunately it will be wrong but it's better than starting over.
+        if 'attributes' in session and\
+            session['attributes']['game_status'] == "in_progress":
                 return handle_answer_request(intent, session)
-        return play_new_game(False)
+        return play_new_game(replay=False)
+
     if intent_name in ("AMAZON.StartOverIntent", "AMAZON.YesIntent"):
-        print("=====StartOverIntent or YesIntent fired...")
-        return play_new_game(True)
+        logger.debug("=====StartOverIntent or YesIntent fired, new game...")
+        return play_new_game(replay=True)
+
     if intent_name == "AMAZON.NoIntent":
-        print("=====NoIntent fired...")
-        # if there's a session and we're in a game treat this as a wrong answer
+        logger.debug("=====NoIntent fired...")
+        # If there's a session and we're in a game treat this as a wrong answer
         if 'attributes' in session:
             if session['attributes']['game_status'] == "in_progress":
                 return handle_answer_request(intent, session)
-        # otherwise end the game
+        # Otherwise end the game
         return play_end_message()
+
     if intent_name in ("AMAZON.StopIntent", "AMAZON.CancelIntent"):
-        print("=====StopIntent or CancelIntent fired")
+        logger.debug("=====StopIntent or CancelIntent fired...")
         return play_end_message()
-    if intent_name == 'AMAZON.HelpIntent':
-        print("=====HelpIntent...")
+
+    if intent_name == "AMAZON.HelpIntent":
+        logger.debug("=====HelpIntent fired...")
         tts = "During the game I'll give you 6 random brain teasers and only 8 "\
             "seconds to anser each one... To make your mind muscles stronger, I "\
             "won't repeat any of the questions, so try to remember all the "\
             "details... You can say 'Start Over' if you'd like a new game, "\
             "or make your guess for the last question..."
-        return speech(tts, session['attributes'], False, None)
-
-def on_session_ended(event_request, session):
-    """when the user ends the session intentionally or timeout"""
-    print("=====on_session_ended requestId=" + event_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-    return play_end_message()
+        return speech(tts, session['attributes'], 
+            should_end_session=False, answered_correctly=None)
